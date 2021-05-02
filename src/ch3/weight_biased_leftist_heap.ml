a6ec0b227ba4234056645b713e73a61f58bfa3d1
lin <linoshitani@gmail.com>
2021-05-02 23:33:59 +0900

    solve exercise 3.4(c)

--- src/ch3/weight_biased_leftist_heap.ml
+++ src/ch3/weight_biased_leftist_heap.ml
@@ -18,11 +18,21 @@ module Make (Element : Ordered_intf.S) = struct
     | E -> 0
     | T (w, _, _, _) -> w
 
-  let make_t w v l r =
-    if weight l <= weight r then
-      T (w, v, r, l)
-    else
-      T (w, v, l, r)
+  (* let make_t w v l r =
+   *   if weight l <= weight r then
+   *     T (w, v, r, l)
+   *   else
+   *     T (w, v, l, r)
+   *
+   * let rec merge t1 t2 =
+   *   match t1, t2 with
+   *   | E, _ -> t2
+   *   | _, E -> t1
+   *   | T (w1, v1, l1, r1), T (w2, v2, l2, r2) ->
+   *     if Elm.(v1 <= v2) then
+   *       make_t (w1 + w2) v1 l1 (merge r1 t2)
+   *     else
+   *       make_t (w1 + w2) v2 l2 (merge r2 t1) *)
 
   let rec merge t1 t2 =
     match t1, t2 with
@@ -30,9 +40,15 @@ module Make (Element : Ordered_intf.S) = struct
     | _, E -> t1
     | T (w1, v1, l1, r1), T (w2, v2, l2, r2) ->
       if Elm.(v1 <= v2) then
-        make_t (w1 + w2) v1 l1 (merge r1 t2)
+        if weight l1 <= w2 + weight r1 then
+          T (w1 + w2, v1, merge r1 t2, l1)
+        else
+          T (w1 + w2, v1, l1, merge r1 t2)
       else
-        make_t (w1 + w2) v2 l2 (merge r2 t1)
+      if weight l2 <= w1 + weight r2 then
+        T (w1 + w2, v2, merge t1 r2, l2)
+      else
+        T (w1 + w2, v2, l2, merge t1 r2)
 
   let insert t ~value = merge t (T (1, value, E, E))
 
